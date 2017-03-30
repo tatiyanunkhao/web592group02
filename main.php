3afa8281d59e9eddaabbe0fc1fea7ac66c3e0ee7
Wachirawut Thamviset <twachikku@gmail.com>
2017-03-30 11:57:01 +0700

    เพิ่มไฟล์

--- main.php
+++ main.php
@@ -1,2 +1,13 @@
-<?php
-    echo 'Hello world!';
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <title></title>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+ <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body role="document">
+	<h1>Group 2</h1>
+</body>
+</html>
