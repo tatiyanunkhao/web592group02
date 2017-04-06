ee8627e312a030a068b96347b1d45f575da2a7cc
Ethchanathip <chanathipsangkhamanee@gmail.com>
2017-04-06 14:43:45 +0700

    Add files via upload

--- main.php
+++ main.php
@@ -1,14 +1,2 @@
-<html lang="en">
-<head>
- <meta charset="UTF-8">
- <title></title>
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
- <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body role="document">
-<h1>Group 2</h1>
-test 123
-</body>
-</html>
+<?php
+    echo 'Hello world!';
